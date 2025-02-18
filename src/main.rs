6a2956fb339b1002cf33c071872fe3cae7ea432b
E-gonito <masterdank47@gmail.com>
2025-02-18 12:36:15 +0000

    Initialise Cargo

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,5 @@ fn main () {
     // This is a comment - println! is a macro that prints text and a newline
     println!("Learning Rust is fun!");
     // You can also print numbers and expressions
-             println!("2 + 2 = {}", 2 + 2);
+    println!("2 + 2 = {}", 2 + 2);
 }
