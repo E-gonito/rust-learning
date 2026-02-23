64171e725541d9e166bb0a7ab2d164afa740b10b
E-gonito <errol.gonito@gmail.com>
2026-02-23 19:41:00 +0000

    feat(hello_world): initalise hello world rust cargo

---
+++ hello_world/target/debug/deps/hello_world-5d9ae6570edba701.d
@@ -0,0 +1,5 @@
+/Users/macbookm1/Code/rust-learning/hello_world/target/debug/deps/hello_world-5d9ae6570edba701.d: src/main.rs
+
+/Users/macbookm1/Code/rust-learning/hello_world/target/debug/deps/libhello_world-5d9ae6570edba701.rmeta: src/main.rs
+
+src/main.rs:
---
+++ hello_world/target/debug/deps/hello_world-ff57a62c3aca9f94.d
@@ -0,0 +1,5 @@
+/Users/macbookm1/Code/rust-learning/hello_world/target/debug/deps/hello_world-ff57a62c3aca9f94.d: src/main.rs
+
+/Users/macbookm1/Code/rust-learning/hello_world/target/debug/deps/hello_world-ff57a62c3aca9f94: src/main.rs
+
+src/main.rs:
